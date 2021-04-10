b1370eae672ea997f62ba55d15599730bcff9316
Harry Jubb <harry.jubb@gmail.com>
2021-04-10 18:01:18 +0100

    Row functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 use std::fmt;
 
+const COMPLETED_SEGMENT: i8 = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9;
+
 pub struct Board {
-    board: Vec<Vec<i8>>,
+    pub board: Vec<Vec<i8>>,
 }
 
 impl Board {
@@ -12,21 +14,30 @@ impl Board {
     }
 }
 
-trait Sudoku {
-    // fn rows(&self) -> Vec<Vec<i8>>;
+pub trait Sudoku {
+    fn rows(&self) -> &[Vec<i8>];
     // fn cols(&self) -> [[i8; 9]; 9];
+    fn rows_valid(&self) -> bool;
+    // fn cols_valid(&self) -> bool;
     fn valid(&self) -> bool;
     fn set_value(&mut self, i: usize, j: usize, value: i8);
 }
 
 impl Sudoku for Board {
-    // fn rows(&self) {
-    //     self.board
-    // }
+    fn rows(&self) -> &[Vec<i8>] {
+        self.board.as_slice()
+    }
 
     // fn cols(&self) -> [[i8; 9]; 9] {
     //     // Vec
     // }
+
+    fn rows_valid(&self) -> bool {
+        self.rows()
+            .iter()
+            .map(|row| row.iter().sum::<i8>() == COMPLETED_SEGMENT)
+            .all(|row| row == true)
+    }
 
     fn valid(&self) -> bool {
         false
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use rudoku::Board;
+use rudoku::{Board, Sudoku};
 
 fn main() {
     let board: Board = Board::new();
     println!("{}", board);
+    println!("Rows valid: {}", board.rows_valid());
 }
